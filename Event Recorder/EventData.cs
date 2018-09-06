56f79a3f3660816ba5a49c0d8071f5d1724740e6
Felipe Martinez <pipernene@gmail.com>
2018-09-07 00:10:49 +0200

    Added things

---
+++ Event Recorder/EventData.cs
@@ -0,0 +1,26 @@
+﻿using LCU.NET.WAMP;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event_Recorder
+{
+    public struct EventData
+    {
+        public DateTime Time { get; }
+        public DateTime RecordingStartTime { get; }
+        public JsonApiEvent JsonEvent { get; }
+
+        public TimeSpan TimeSinceStart => Time.Subtract(RecordingStartTime);
+        
+        public EventData(DateTime recordingStartTime, JsonApiEvent jsonEvent)
+        {
+            this.Time = DateTime.Now;
+            this.RecordingStartTime = recordingStartTime;
+            this.JsonEvent = jsonEvent;
+        }
+    }
+}
--- Event Recorder/MainWindow.xaml.cs
+++ Event Recorder/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using LCU.NET;
+using LCU.NET.WAMP;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +24,39 @@ namespace Event_Recorder
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DateTime StartTime;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.StartTime = DateTime.Now;
+            LeagueClient.Default.BeginTryInit();
+
+            LeagueSocket.SubscribeRaw(Handler);
+        }
+
+        private void Handler(JsonApiEvent obj)
+        {
+            Dispatcher.Invoke(() => Events.Items.Add(new EventData(this.StartTime, obj)));
+        }
+        
+        private void CopyURI(object sender, RoutedEventArgs e)
+        {
+            var ev = (EventData)Events.SelectedItem;
+            Clipboard.SetText(ev.JsonEvent.URI);
+        }
+
+        private void CopyData(object sender, RoutedEventArgs e)
+        {
+            var ev = (EventData)Events.SelectedItem;
+            Clipboard.SetText(ev.JsonEvent.Data.ToString(Formatting.None));
+        }
+
+        private void CopyDataIndented(object sender, RoutedEventArgs e)
+        {
+            var ev = (EventData)Events.SelectedItem;
+            Clipboard.SetText(ev.JsonEvent.Data.ToString());
         }
     }
 }
